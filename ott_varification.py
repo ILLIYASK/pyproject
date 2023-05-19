ee97603902349020316e0e706a1700d4196f2aba
ILLIYASK <Illiyask26061999@gmail.com>
2023-05-19 13:42:46 +0530

    timer added

--- ott_varification.py
+++ ott_varification.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from twilio.rest import Client
 from tkinter import messagebox
+import time
 
 account_sid = "ACfc6ea1c9a9fb8ccb1512129f0817a3bb"
 auth_token = "f629238878fc0de07c3af46918456af8"
@@ -15,18 +16,38 @@ class OtpVarifier(Tk):
         self.config(bg="white")
         self.resizable(0,0)
         self.client=Client(account_sid,auth_token)
+        self.time=120
 
 
     def send(self):
         self.verification = self.client.verify.v2.services(verify_sid) \
             .verifications \
             .create(to=verified_number, channel="sms")
+    def update(self):
+        self.time=self.time-1
+        print(self.time)
+        if self.time>=-1:
+            self.timer_label()
+        else:
+            messagebox.showinfo("showinfo","time out ")
+            window.destroy()
 
     def Labels(self):
         self.c=Canvas(self,bg="light blue",width=400,height=280)
         self.c.place(x=290,y=120)
         self.upper=Frame(self,bg="gray",width=1500,height=130)
         self.upper.place(x=0,y=0)
+
+    def timer_label(self):
+        self.time-=1
+        if self.time>=0:
+            self.timer=Label(self,bg="black",fg="white",width=44,height=3,text=f" 00 : {self.time//60:>02}: {self.time%60:>02}")
+            self.timer.place(x=300,y=450)
+            self.timer.after(1000,self.timer_label)
+        else:
+            window.destroy()
+
+
     def entry(self):
         self.e=Entry(self,bg="white",width=20)
         self.e.place(x=400,y=200)
@@ -51,4 +72,5 @@ if __name__== "__main__" :
     window.Labels()
     window.entry()
     window.buuton()
+    window.timer_label()
     window.mainloop()
